34fdb1b4948fd8ed3d237673dedec13bdf16c3ba
ilhamCode <92145348+ilhamCode@users.noreply.github.com>
2021-11-01 08:33:36 +0400

    Add files via upload

---
+++ oct-29(task 3)/oct-29(task 3)/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oct_29_task_3_
+{
+    class Box
+    {
+      public  int width;
+       public  int length;
+       public  int height;
+        public Box(int width,int length,int height)
+        {
+            this.width = width;
+            this.length = length;
+            this.height = height;
+        }
+
+        public int volumeOfBox(Box qutu)
+        {
+            int volume = qutu.height * qutu.length * qutu.width;
+            return volume;
+        }
+    }
+}
---
+++ oct-29(task 3)/oct-29(task 3)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace oct_29_task_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box qutu = new Box(5,6,7);
+           int volume =  qutu.volumeOfBox(qutu);
+            Console.WriteLine(volume);
+        }
+        
+    }
+}
