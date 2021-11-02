514ae80ee69b7a2bcbe3852a35619c9b1b386935
ilhamCode <92145348+ilhamCode@users.noreply.github.com>
2021-11-02 10:24:51 +0400

    Add files via upload

---
+++ nov-1(task 3)/nov-1(task 3)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace nov_1_task_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            subshort("Rasim is a masculine given name of Arabic origin meaning 'planner','architect'.");
+        }
+
+        static void subshort(string sentence)
+        {
+            string newstr;
+
+            if (sentence.Length > 50)
+            {
+
+                newstr=sentence.Substring(0,50);
+                Console.WriteLine(newstr);
+               
+            }
+            else
+            {
+                Console.WriteLine(sentence);
+
+
+            }
+
+        }
+
+
+    }
+}
