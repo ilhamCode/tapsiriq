fb6eb3bed694d194650440bbc9bb3371fdc68fa3
ilhamCode <92145348+ilhamCode@users.noreply.github.com>
2021-11-10 07:24:54 +0400

    Add files via upload

---
+++ hospital/hospital/models/palata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hospital.models
+{
+   public class palata
+    {
+        public int id { get; set; }
+        public string branch { get; set; }
+        public int yataqSayi { get; set; }
+    }
+}
---
+++ hospital/hospital/models/xeste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hospital.models
+{
+   public class xeste
+    {
+        public int id { get; set; }
+        public string Ad { get; set; }
+        public string soyad { get; set; }
+        public int yash { get; set; }
+        public string xestelik { get; set; }
+    }
+}
